b1c1684b179a4c0739fb60b1dce5dd2ac83d495e
David Zumbrunnen <dz@oxon.ch>
2013-07-03 14:29:10 +0200

    Symlink for files dir and configuration.yml added

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,8 @@
 # Symlink shared dirs
 on_app_servers do
-  run "ln -nfs #{shared_path}/plugin_assets #{release_path}/public/plugin_assets"
-  run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   run "ln -nfs #{shared_path}/plugins #{release_path}/plugins"
+  run "ln -nfs #{shared_path}/files #{release_path}/files"
+  run "ln -nfs #{shared_path}/plugin_assets #{release_path}/public/plugin_assets"
+  run "ln -nfs #{shared_path}/configuration.yml #{release_path}/config/configuration.yml"
+  run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
 end

e7f2cd0b8d137b07a52840f2d9fd0efbd0987479
David Zumbrunnen <dz@oxon.ch>
2013-07-15 09:19:41 +0200

    OpenID config: store defined

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,4 +5,5 @@ on_app_servers do
   run "ln -nfs #{shared_path}/plugin_assets #{release_path}/public/plugin_assets"
   run "ln -nfs #{shared_path}/configuration.yml #{release_path}/config/configuration.yml"
   run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
+  run "ln -nfs #{shared_path}/openid.rb #{release_path}/config/initializers/openid.rb"
 end

07778f73fe31a7f6cdf34bf992e1221ab621cead
David Zumbrunnen <dz@oxon.ch>
2013-10-09 11:30:29 +0200

    Replaced deprecated EY config vars

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,9 +1,9 @@
 # Symlink shared dirs
 on_app_servers do
-  run "ln -nfs #{shared_path}/plugins #{release_path}/plugins"
-  run "ln -nfs #{shared_path}/files #{release_path}/files"
-  run "ln -nfs #{shared_path}/plugin_assets #{release_path}/public/plugin_assets"
-  run "ln -nfs #{shared_path}/configuration.yml #{release_path}/config/configuration.yml"
-  run "ln -nfs #{shared_path}/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
-  run "ln -nfs #{shared_path}/openid.rb #{release_path}/config/initializers/openid.rb"
+  run "ln -nfs #{config.shared_path}/plugins #{config.release_path}/plugins"
+  run "ln -nfs #{config.shared_path}/files #{config.release_path}/files"
+  run "ln -nfs #{config.shared_path}/plugin_assets #{config.release_path}/public/plugin_assets"
+  run "ln -nfs #{config.shared_path}/configuration.yml #{config.release_path}/config/configuration.yml"
+  run "ln -nfs #{config.shared_path}/secret_token.rb #{config.release_path}/config/initializers/secret_token.rb"
+  run "ln -nfs #{config.shared_path}/openid.rb #{config.release_path}/config/initializers/openid.rb"
 end
